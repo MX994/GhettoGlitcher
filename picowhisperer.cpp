cfcd94ba2ab99fa161d9573636c9e9ddbc69a952
Tensor497 <ikeita@umass.edu>
2023-10-09 16:10:14 -0400

    Initial commit.

---
+++ picowhisperer.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+#include "picowhisperer.hpp"
+extern "C" { 
+    int main() {
+        while (true) {
+            puts("YEO\n");
+        }
+        return 0;
+    }
+}
